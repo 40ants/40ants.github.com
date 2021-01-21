199d1f5aa9116af87fcdebcdf8e3b94a3966d789
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-01-21 22:07:47 +0300

    Added test2 page with MVP list of projects.

---
+++ plugins/spinneret.lisp
@@ -0,0 +1,23 @@
+(eval-when (:compile-toplevel :load-toplevel)
+  (ql:quickload '(spinneret
+                  ;; We need this lib on the pages
+                  github)
+                :silent t))
+
+(defpackage :coleslaw-spinneret
+  (:use #:cl #:spinneret)
+  (:import-from #:coleslaw :render-text)
+  (:export #:enable))
+
+(in-package :coleslaw-spinneret)
+
+
+(defmethod render-text (text (format (eql :spinneret)))
+  (let* (;; (*package* (find-package '#:coleslaw-cl-who-renderer))
+         (sexps
+           (with-input-from-string (s text)
+             (uiop:slurp-stream-forms s))))
+    (eval `(spinneret:with-html-string ()
+             ,@sexps))))
+
+(defun enable ())
